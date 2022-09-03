b6c4f544892d9eda4446a6bd1cc67fbc46019f99
Renzo Campisi <campisirenzo0@gmail.com>
2022-09-03 19:37:06 -0300

    MCGA 02

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require('express')
-//const products = require('./data/products.json')
+const products = require('./data/products.json')
+const fs = require("fs");
 const app = express()
-const port = 3000
+const morgan = require('morgan')
+
+//Settings
+app.set('port', 3000)
+
+//Middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+app.post("/products/add",(req, res) => {
+    console.log(req.body);
+    const newProd = {
+        id: req.body.id,
+        name: req.body.name,
+        price: req.body.price,
+        stock: req.body.stock
+    }
+    if (!newProd.id) {
+      res.sendStatus(400)
+    }
+    products.push(newProd)
+    fs.writeFile("./data/products.json", JSON.stringify(products),(err) => {
+
+    })
+    res.json(newProd)
+} )
+
+app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'))
 })
